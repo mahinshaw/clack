40662542675aaa2394167fe974ae9087e4875629
mahinshaw <mahinshaw@gmail.com>
2015-03-19 22:25:10 -0400

    Use core.async for sending and recieving messages.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Clack
+
+A bot for slack. The goal was to use the rtm api for commication.
+
+## TODO
+* Clean up slack namespace
+  * Seperate websocket connection from message/event handling.
--- src/clj/clack/slack.clj
+++ src/clj/clack/slack.clj
@@ -1,5 +1,7 @@
 (ns clack.slack
   (:require [clojure.java.io :as io]
+            [clojure.core.match :refer [match]]
+            [clojure.core.async :as a :refer [chan]]
             [clojure.edn :as edn]
             [clojure.string :as str]
             [cheshire.core :as json]
@@ -8,16 +10,39 @@
             [clack.clack-util.doctor :refer [talk->Doctor]]))
 
 ;;; Helpers
-(defn find-first [pred coll]
+(defn find-first
+  "Returns the first value in the collection that matches the predicate."
+  [pred coll]
   (first (filter pred coll)))
 ;;; Helpers
+
+(def cache-limit 10)
 
 (def config
   "Configuration file for clack"
   (-> "config.edn" io/resource slurp edn/read-string))
 
+(def message-cache (atom []))
+
+(defn add-message-to-cache
+  "Adds the passed message to the cache, limiting the cache to the cache-limit."
+  [msg]
+  (when (<= cache-limit (count @message-cache))
+    (reset! message-cache (vec (take-last (dec cache-limit) @message-cache))))
+  (peek (swap! message-cache conj msg)))
+
+(defn msg->json
+  "Takes a message and returns the json string, caching the message as a map."
+  [msg]
+  (-> msg add-message-to-cache json/generate-string))
+
 (def conn-settings (atom {}))
 (def ws-connection (atom nil))
+
+;;; Channels
+(def recv-chan (chan))
+;; buffer send channel, to mitigate rate limit issues. Start with 10.
+(def send-chan (chan cache-limit))
 
 (defn self-settings []
   (:self @conn-settings))
@@ -31,11 +56,6 @@
   (map (fn [{:keys [id user]}]
          {:id id :user-id user})
        (:ims @conn-settings)))
-
-(defn user->DM
-  "Take a user id and return a direct channel"
-  [user]
-  (find-first #(= user (:user-id %)) (direct-chat-ids)))
 
 (defn get-name [id key]
   (:name (find-first
@@ -79,15 +99,21 @@
   (let [id (str/replace txt #"[\@\:]" "")]
     (str "@" (get-user-name id) ":")))
 
-(defn direct-at-user [user-id]
+(defn direct-at-user
+  "Direct the message to a user with <@{user-id}|{user-name}>"
+  [user-id]
   (str "<@" user-id "|" (get-user-name user-id) ">:"))
 
-(defn direct-at-channel [chan-id]
+(defn direct-at-channel
+  "Direct the message to a channel with <@{chan-id}|{channel-name}>"
+  [chan-id]
   (str "<@" chan-id "|" (get-chan-name chan-id) ">:"))
 
 (def test-string "<@U03PGH4PL> hello")
 (def test-string2 "<@U03PGH4PL>: hello")
-(defn directed-text [txt]
+(defn directed-text
+  "Attempt to find directed text message, and split the message into the @<...> and message."
+  [txt]
   (if-let [to (re-find #"\<\@\w+\>\:?" txt)]
     [(-> to construct-at replace-at) (strip-at txt)]
     [nil txt]))
@@ -99,11 +125,17 @@
   ([type msg channel]
    {:type type :id (str (swap! message-id inc)) :text (str msg) :channel channel}))
 
-(defn send-slack-message [connection msg-map]
-  (when-let [ws-client @@connection]
-    (s/put! ws-client (json/generate-string msg-map))))
+(defn send-slack-message
+  "Send a message in the form of a clojure map via the given connection.
+  The connection should be a manifold stream."
+  [connection msg]
+  (clojure.pprint/pprint msg)
+  (when-let [ws-client @connection]
+    (do
+      (println (msg->json msg))
+      (s/put! ws-client (msg->json msg)))))
 
-(defn respond-msg
+(defn respond-message
   "Responds to message event type."
   [msg]
   (let [{:keys [type subtype hidden channel user text ts team]
@@ -115,23 +147,24 @@
         ;; They should only refer to direct contact with slack, either via @clack or direct channel "D03..."
         (let [[to txt] (directed-text text)]
           (if to
-            (send-slack-message
-             ws-connection
-             (slack-message-map type (str (direct-at-user user) " " (talk->Doctor txt)) channel))
+            (do
+              (println "Put message")
+              (a/put!
+               send-chan
+               (slack-message-map type (str (direct-at-user user) " " (talk->Doctor txt)) channel)))
             (when (direct-with-user? channel user)
-              (send-slack-message
-               ws-connection
-               (slack-message-map type (talk->Doctor text) channel)))))))))
+              (do
+                (println "Put message")
+                (a/put!
+                 send-chan
+                 (slack-message-map type (talk->Doctor text) channel))))))))))
 
 (defn consumer
   "Take some data, parse it and return response if necessary"
   [data]
   (let [recv (json/parse-string data true)]
     (clojure.pprint/pprint recv)
-    (let [type (:type recv)]
-      (case type
-        "message" (respond-msg recv)
-        "hello"))))
+    (a/put! recv-chan recv)))
 
 (defn set-connection []
   (do (reset! conn-settings (slack-authenticate (:token config)))
@@ -150,10 +183,40 @@
   (s/consume consumer @conn)
   (s/on-closed @conn on-closed))
 
-(defn start-client []
+;; TODO: Is this necessary? Ideally this channel will send recieved messages upstream
+;; to the dashboard etc.  But for now is it useful?
+(defn start-recv-loop
+  "Loop on recieved messages"
+  []
+  (a/go-loop []
+    (println "Waiting to recieve message")
+    (let [recv (a/<! recv-chan )
+          type (:type recv)]
+      (println "Message Recieved")
+      (case type
+        "message" (respond-message recv)
+        "hello")
+      (recur))))
+
+(defn start-send-loop
+  "Starts a loop with a 1 sec timeout, because the rtm limit is 1msg/sec."
+  [conn]
+  (a/go-loop []
+    (a/<! (a/timeout 1000))
+    (let [msg (a/<! send-chan)]
+      (send-slack-message
+       conn
+       msg)
+      (recur))))
+
+(defn start-client
+  "Starts the websocket based client for connecting to slack."
+  []
   (do
     (set-connection)
-    (setup-connection-handlers @ws-connection consumer on-closed)))
+    (setup-connection-handlers @ws-connection consumer on-closed)
+    (start-recv-loop)
+    (start-send-loop @ws-connection)))
 
 (defn stop-client
   "Sets the on-close event to nil, and closes the connection."
